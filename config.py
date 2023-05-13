d8d8537478457d466d0cb105fdae6ac36ac8c009
hodlmod.eth <93076644+zaivanza@users.noreply.github.com>
2023-05-13 08:43:46 +0500

    Update config.py

--- config.py
+++ config.py
@@ -17,6 +17,7 @@ import decimal
 
 from setting import *
 from data.abi.abi import *
+from data.rpc import DATA
 
 outfile = ''
 with open(f"{outfile}data/abi/erc20.json", "r") as file:
@@ -40,29 +41,6 @@ with open(f"{outfile}data/proxies.txt", "r") as f:
 STR_DONE    = '✅ ' 
 STR_CANCEL  = '❌ '
     
-# меняем рпс на свои
-DATA = {
-    'ethereum'      : {'rpc': 'https://rpc.ankr.com/eth', 'scan': 'https://etherscan.io/tx', 'token': 'ETH'},
-
-    'optimism'      : {'rpc': 'https://rpc.ankr.com/optimism', 'scan': 'https://optimistic.etherscan.io/tx', 'token': 'ETH'},
-
-    'bsc'           : {'rpc': 'https://rpc.ankr.com/bsc', 'scan': 'https://bscscan.com/tx', 'token': 'BNB'},
-
-    'polygon'       : {'rpc': 'https://rpc.ankr.com/polygon', 'scan': 'https://polygonscan.com/tx', 'token': 'MATIC'},
-
-    'polygon_zkevm' : {'rpc': 'https://zkevm-rpc.com', 'scan': 'https://zkevm.polygonscan.com/tx', 'token': 'ETH'},
-
-    'arbitrum'      : {'rpc': 'https://rpc.ankr.com/arbitrum', 'scan': 'https://arbiscan.io/tx', 'token': 'ETH'},
-
-    'avalanche'     : {'rpc': 'https://rpc.ankr.com/avalanche', 'scan': 'https://snowtrace.io/tx', 'token': 'AVAX'},
-
-    'fantom'        : {'rpc': 'https://rpc.ankr.com/fantom', 'scan': 'https://ftmscan.com/tx', 'token': 'FTM'},
-
-    'nova'          : {'rpc': 'https://nova.arbitrum.io/rpc', 'scan': 'https://nova.arbiscan.io/tx', 'token': 'ETH'},
-
-    'zksync'        : {'rpc': 'https://mainnet.era.zksync.io', 'scan': 'https://explorer.zksync.io/tx', 'token': 'ETH'},
-}
-
 def intToDecimal(qty, decimal):
     return int(qty * int("".join(["1"] + ["0"]*decimal)))
 
