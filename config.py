bf7dc70c6dd69553141554994c0663617fd4745d
hodlmod.eth <93076644+zaivanza@users.noreply.github.com>
2023-06-05 09:35:05 +0500

    Update config.py

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ from pyuseragents import random as random_useragent
 from tabulate import tabulate
 import math
 import decimal
+from uniswap import Uniswap
 
 from setting import *
 from data.abi.abi import *
@@ -173,6 +174,17 @@ SUSHISWAP_CONTRACTS = {
     'starknet'      : '',
 }
 
+BUNGEE_REFUEL_CONTRACTS = {
+    'ethereum'      : '0xb584D4bE1A5470CA1a8778E9B86c81e165204599',
+    'optimism'      : '0x5800249621da520adfdca16da20d8a5fc0f814d8',
+    'bsc'           : '0xbe51d38547992293c89cc589105784ab60b004a9',
+    'arbitrum'      : '0xc0e02aa55d10e38855e13b64a8e1387a04681a00',
+    'polygon'       : '0xAC313d7491910516E06FBfC2A0b5BB49bb072D91',
+    'polygon_zkevm' : '0x555a64968e4803e27669d64e349ef3d18fca0895',
+    'zksync'        : '0x7Ee459D7fDe8b4a3C22b9c8C7aa52AbadDd9fFD5',
+    'avalanche'     : '0x040993fbf458b95871cd2d73ee2e09f4af6d56bb',
+}
+
 
 text1 = '''
  /$$   /$$  /$$$$$$  /$$$$$$$  /$$       /$$      /$$  /$$$$$$  /$$$$$$$ 
@@ -253,7 +265,13 @@ def get_prices():
         time.sleep(1)
         return get_prices()
     
+def get_bungee_data():
+    url = "https://refuel.socket.tech/chains"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        return data
+    
 PRICES_NATIVE   = get_prices()
 WALLET_PROXIES  = get_wallet_proxies()
-
-
+BUNGEE_LIMITS   = get_bungee_data()
