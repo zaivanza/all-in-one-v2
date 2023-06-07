26b8d44b8c29b7c29016cb50ce333bdeafd92de5
hodlmod.eth <93076644+zaivanza@users.noreply.github.com>
2023-06-07 11:00:46 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -77,41 +77,26 @@ def check_status_tx(chain, tx_hash):
 
 def add_gas_limit(web3, contract_txn):
 
-    try:
-        value = contract_txn['value']
-        contract_txn['value'] = 0
-        pluser = [1.02, 1.05]
-        gasLimit = web3.eth.estimate_gas(contract_txn)
-        contract_txn['gas'] = int(gasLimit * random.uniform(pluser[0], pluser[1]))
-        # logger.info(f"gasLimit : {contract_txn['gas']}")
-    except Exception as error: 
-        contract_txn['gas'] = random.randint(2000000, 3000000)
-        logger.info(f"estimate_gas error : {error}. random gasLimit : {contract_txn['gas']}")
+    value = contract_txn['value']
+    contract_txn['value'] = 0
+    pluser = [1.02, 1.05]
+    gasLimit = web3.eth.estimate_gas(contract_txn)
+    contract_txn['gas'] = int(gasLimit * random.uniform(pluser[0], pluser[1]))
 
     contract_txn['value'] = value
     return contract_txn
 
 def add_gas_limit_layerzero(web3, contract_txn):
 
-    try:
-        pluser = [1.05, 1.07]
-        gasLimit = web3.eth.estimate_gas(contract_txn)
-        contract_txn['gas'] = int(gasLimit * random.uniform(pluser[0], pluser[1]))
-        # logger.info(f"gasLimit : {contract_txn['gas']}")
-    except Exception as error: 
-        contract_txn['gas'] = random.randint(2000000, 3000000)
-        logger.info(f"estimate_gas error : {error}. random gasLimit : {contract_txn['gas']}")
-
+    pluser = [1.05, 1.07]
+    gasLimit = web3.eth.estimate_gas(contract_txn)
+    contract_txn['gas'] = int(gasLimit * random.uniform(pluser[0], pluser[1]))
     return contract_txn
 
 def add_gas_price(web3, contract_txn):
 
-    try:
-        gas_price = web3.eth.gas_price
-        contract_txn['gasPrice'] = int(gas_price * random.uniform(1.01, 1.02))
-    except Exception as error: 
-        logger.error(error)
-
+    gas_price = web3.eth.gas_price
+    contract_txn['gasPrice'] = int(gas_price * random.uniform(1.01, 1.02))
     return contract_txn
 
 def round_to(num, digits=3):
@@ -384,176 +369,6 @@ def transfer(privatekey, retry=0):
             logger.info(f'try again | {wallet}')
             sleeping(10, 10)
             transfer(privatekey, retry+1)
-        else:
-            list_send.append(f'{STR_CANCEL}{module_str}')
-
-def get_api_call_data(url):
-
-    def try_get_with_proxy():
-        try:
-            proxy = random.choice(PROXIES)
-            # cprint(proxy, 'yellow')
-            proxies = {
-                'http'  : proxy,
-                'https' : proxy,
-            }
-            call_data = requests.get(url, proxies=proxies)
-            return call_data
-        except Exception as error:
-            logger.error(error)
-            call_data = requests.get(url)
-            return call_data
-
-    call_data = requests.get(url)
-
-    # cprint(f'call_data.status_code : {call_data.status_code}', 'blue')
-
-    if call_data.status_code == 200:
-        api_data = call_data.json()
-        return api_data
-    
-    else:
-
-        call_data = try_get_with_proxy()
-
-        if call_data.status_code == 200:
-            api_data = call_data.json()
-            return api_data
-        
-        else:
-
-            try:
-                api_data = call_data.json()
-                logger.error(api_data['description'])
-                return False
-            
-            except ValueError as error:
-                logger.error(error)
-                time.sleep(1)
-                return get_api_call_data(url)
-            
-            except Exception as error:
-                logger.error(error)
-                time.sleep(1)
-                return get_api_call_data(url)
-
-def inch_swap(privatekey, retry=0):
-        
-    try:
-
-        module_str = '1inch_swap'
-        logger.info(module_str)
-
-        base_url = 'https://api-defillama.1inch.io'
-        # base_url = 'https://api.1inch.io'
-        inch_version = 5
-
-        chain, swap_all_balance, min_amount_swap, keep_value_from, keep_value_to, amount_from, amount_to, from_token_address, to_token_address, slippage, divider = value_1inch_swap()
-
-        keep_value = round(random.uniform(keep_value_from, keep_value_to), 8)
-
-        if chain == 'zksync': divider = divider
-        else: divider = 1
-
-        web3 = get_web3(chain, privatekey)
-        chain_id = web3.eth.chain_id
-
-        if from_token_address == '': 
-            from_token_address = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
-            from_decimals = 18
-            from_symbol = DATA[chain]['token']
-        else:
-            from_token_contract, from_decimals, from_symbol = check_data_token(chain, from_token_address)
-
-        if to_token_address   == '': 
-            to_token_address   = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
-            to_symbol = DATA[chain]['token']
-        else:
-            to_token_contract, to_decimals, to_symbol = check_data_token(chain, to_token_address)
-
-        account = web3.eth.account.from_key(privatekey)
-        wallet  = account.address
-
-        if swap_all_balance == True: amount = check_balance(privatekey, chain, from_token_address) - keep_value
-        else: amount = round(random.uniform(amount_from, amount_to), 8)
-
-        amount = amount*0.999
-        amount_to_swap = intToDecimal(amount, from_decimals) 
-
-        spender_json    = get_api_call_data(f'{base_url}/v{inch_version}.0/{chain_id}/approve/spender')
-        spender         = Web3.to_checksum_address(spender_json['address'])
-
-        # если токен не нативный, тогда проверяем апрув и если он меньше нужного, делаем апруваем
-        if from_token_address != '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE':
-            approve_(amount_to_swap, privatekey, chain, from_token_address, spender)
-
-        _1inchurl = f'{base_url}/v{inch_version}.0/{chain_id}/swap?fromTokenAddress={from_token_address}&toTokenAddress={to_token_address}&amount={amount_to_swap}&fromAddress={wallet}&slippage={slippage}'
-        json_data = get_api_call_data(_1inchurl)
-
-        # cprint(json_data, 'blue')
-
-        if json_data == False: 
-            
-            logger.info('failed to swap in 1inch')
-
-        else:
-
-            # cprint(json_data, 'yellow')
-
-            tx  = json_data['tx']
-
-            tx['chainId']   = chain_id
-            tx['nonce']     = web3.eth.get_transaction_count(wallet)
-            tx['to']        = Web3.to_checksum_address(tx['to'])
-            tx['gasPrice']  = int(tx['gasPrice'])
-            tx['gas']       = int(int(tx['gas']) / divider)
-            tx['value']     = int(tx['value'])
-
-            # cprint(tx, 'blue')
-
-            if chain == 'bsc':
-                tx['gasPrice'] = random.randint(1000000000, 1050000000) # специально ставим 1 гвей, так транза будет дешевле
-
-            # смотрим газ, если выше выставленного значения : спим
-            total_fee   = int(tx['gas'] * tx['gasPrice'])
-            is_fee      = checker_total_fee(chain, total_fee)
-            if is_fee   == False: return inch_swap(privatekey, retry)
-
-            if amount >= min_amount_swap:
-                    
-                tx_hash     = sign_tx(web3, tx, privatekey)
-                tx_link     = f'{DATA[chain]["scan"]}/{tx_hash}'
-
-                module_str = f'1inch_swap : {round_to(amount)} {from_symbol} => {to_symbol}'
-
-                status  = check_status_tx(chain, tx_hash)
-
-                if status == 1:
-                    logger.success(f'{module_str} | {tx_link}')
-                    list_send.append(f'{STR_DONE}{module_str}')
-                else:
-                    logger.error(f'{module_str} | tx is failed | {tx_link}')
-                    if retry < RETRY:
-                        logger.info(f'try again in 10 sec.')
-                        sleeping(10, 10)
-                        inch_swap(privatekey, retry+1)
-
-            else:
-                logger.error(f"{module_str} : can't swap : {amount} (amount) < {min_amount_swap} (min_amount_swap)")
-                list_send.append(f'{STR_CANCEL}{module_str} : {amount} less {min_amount_swap}')
-
-    except KeyError:
-        logger.error(json_data['description'])
-        module_str = '1inch_swap'
-        list_send.append(f'{STR_CANCEL}{module_str}')
-
-    except Exception as error:
-        module_str = '1inch_swap'
-        logger.error(f'{module_str} | error : {error}')
-        if retry < RETRY:
-            logger.info(f'try again in 10 sec.')
-            sleeping(10, 10)
-            inch_swap(privatekey, retry+1)
         else:
             list_send.append(f'{STR_CANCEL}{module_str}')
 
@@ -1464,19 +1279,15 @@ def zeroX_swap(privatekey, retry=0):
             contract_txn = {
                 'from'      : wallet,
                 'chainId'   : web3.eth.chain_id,
-                'gasPrice'  : 0,
+                'gasPrice'  : int(json_data[0]['gasPrice']),
                 'nonce'     : web3.eth.get_transaction_count(wallet),
-                'gas'       : 0,
+                'gas'       : int(json_data[0]['gas']),
                 'to'        : Web3.to_checksum_address(json_data[0]['to']),
                 'data'      : json_data[0]['data'],
                 'value'     : int(json_data[0]['value']),
             }
 
-            if chain == 'bsc':
-                contract_txn['gasPrice'] = random.randint(1000000000, 1050000000) # специально ставим 1 гвей, так транза будет дешевле
-            else:
-                contract_txn = add_gas_price(web3, contract_txn)
-            contract_txn = add_gas_limit(web3, contract_txn)
+            contract_txn['gas'] = int(contract_txn['gas'] * 1.3)
 
             # смотрим газ, если выше выставленного значения : спим
             total_fee   = int(contract_txn['gas'] * contract_txn['gasPrice'])
@@ -1635,6 +1446,4 @@ def bungee_refuel(privatekey, retry=0):
             bungee_refuel(privatekey, retry+1)
         else:
             list_send.append(f'{STR_CANCEL}{module_str}')
-
-
 
