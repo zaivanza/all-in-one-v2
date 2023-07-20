1c2c75081fc37926aae025fba45d97b149184707
hodlmod.eth <zasara1980@gmail.com>
2023-07-20 18:37:25 +0500

    add to_chains

--- setting.py
+++ setting.py
@@ -423,7 +423,7 @@ def value_merkly_refuel():
     внимание! сначала посмотри максималку руками, только потом запускай скрипт, иначе можешь потерять свои $.
 
     from_chains : optimism | bsc | polygon | arbitrum | avalanche | fantom | celo | zksync | polygon_zkevm | nova | harmony | gnosis | core
-    to_chains   : optimism | bsc | polygon | arbitrum | avalanche | fantom | celo | meter | canto | polygon_zkevm | nova | harmony | moonbeam | moonriver | metis | kava 
+    to_chains   : avalanche | ethereum | bsc | arbitrum | optimism | fantom | harmony | celo | moonbeam | fuse | gnosis | klaytn | metis | core | polygon_zkevm | canto | zksync | moonriver | tenet | nova | kava | meter
     '''
 
     from_chains = ['bsc', 'polygon', 'arbitrum', 'avalanche'] # запиши сюда сети, с которых хочешь делать refuel (>= 1 сети)
